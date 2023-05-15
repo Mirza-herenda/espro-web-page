b5243f1c64a86c2ec300126673c36524547237ad
Mirza <herendamirza87@gmail.com>
2023-05-15 05:55:25 +0200

    adding movie modal

--- src/components/MoviePart.js
+++ src/components/MoviePart.js
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/MoviePart.css";
 import img1 from "../components/Assets/images/suv.jpg";
 import { Button } from "@mui/material";
 import playBtn from "../components/Assets/icons/play-button.png";
+import CloseIcon from "@mui/icons-material/Close";
+import PlayCircleOutlineRoundedIcon from "@mui/icons-material/PlayCircleOutlineRounded";
 
-function MoviePart() {
+const MoviePart = () => {
+  const [openModal, setOpenModal] = React.useState(false);
+  const [stopZoom, setstopZoom] = useState("zoom 5s infinite alternate;");
+
+  const PlayMovie = () => {
+    setOpenModal(true);
+    setstopZoom("none");
+  };
   return (
     <div className="MainWrapper">
       <div className="textDiv">
@@ -12,7 +21,7 @@ function MoviePart() {
         <h1 className="text1">FRESH GROUNDS. 01</h1>
       </div>
       <div className="imageDiv">
-        <img className="img11" src={img1}></img>
+        <img className="img11" src={img1} style={{ animation: stopZoom }}></img>
         <div className="textDiv2">
           <h4 className="text3">EPISODE 01: THE GREAT MIGRATION</h4>
           <h2 className="text4">FRESH GROUNDS</h2>
@@ -21,14 +30,25 @@ function MoviePart() {
             nonumy eirmod tempor invidunt ut labore et.
           </h5>
 
-          <Button className="playBtn">
+          <Button className="playBtn" onClick={PlayMovie}>
             {" "}
             <img className="img12" src={playBtn}></img> WATCH FILM
           </Button>
         </div>
       </div>
+      {openModal && (
+        <div className="modalBackground" onClick={() => setOpenModal(false)}>
+          <div className="modalContent">
+            <img className="modalImage" />
+            <Button className="closeBtn" style={{ color: "white" }}>
+              X
+            </Button>
+            <PlayCircleOutlineRoundedIcon className="startMovieBtn"></PlayCircleOutlineRoundedIcon>
+          </div>
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default MoviePart;
